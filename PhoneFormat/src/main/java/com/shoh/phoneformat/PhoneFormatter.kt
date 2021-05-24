fc7815db7201f30b994c5ed2f58d813570b18bc1
shohpolat <39366434+shohpolat@users.noreply.github.com>
2021-05-24 13:21:00 +0500

    Initial commit

---
+++ PhoneFormat/src/main/java/com/shoh/phoneformat/PhoneFormatter.kt
@@ -0,0 +1,4 @@
+package com.shoh.phoneformat
+
+class PhoneFormatter {
+}
---
+++ app/src/main/java/com/shoh/test/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.shoh.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shoh.test.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.phoneFormatter.setMask("+### ### ##","+123")
+
+    }
+}
