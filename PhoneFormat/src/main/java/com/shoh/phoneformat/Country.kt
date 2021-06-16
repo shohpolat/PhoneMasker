202ec8d408d1fe7b7336472038099d22525b32f0
shohpolat <39366434+shohpolat@users.noreply.github.com>
2021-06-16 12:27:16 +0500

    latest commmit

--- PhoneFormat/src/main/java/com/shoh/phoneformat/Country.kt
+++ PhoneFormat/src/main/java/com/shoh/phoneformat/Country.kt
@@ -2,45 +2,15 @@ package com.shoh.phoneformat
 
 import android.os.Parcel
 import android.os.Parcelable
+import androidx.versionedparcelable.ParcelField
+import com.google.android.material.internal.ParcelableSparseArray
+import java.io.Serializable
 
 data class Country(
     var countryName: String? = null,
     var phoneMask: String? = null,
     var alpha2code: String? = null,
     var flag: String? = null,
-    var prefixNumber: String? = null,
+    var prefixNumber: ArrayList<String>? = null,
     var alpha3code: String? = null
-):Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(countryName)
-        parcel.writeString(phoneMask)
-        parcel.writeString(alpha2code)
-        parcel.writeString(flag)
-        parcel.writeString(prefixNumber)
-        parcel.writeString(alpha3code)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Country> {
-        override fun createFromParcel(parcel: Parcel): Country {
-            return Country(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Country?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+):Serializable
